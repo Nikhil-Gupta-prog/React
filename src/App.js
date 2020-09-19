72f76d22fcbf6ebe7530249dce29584a0775503e
Nikhil <nikhilgupta122000@gmail.com>
2020-09-19 15:20:36 -0700

    Add basic Components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from "reactstrap";
 import './App.css';
+import "./components/Menucomponents"
+import Menu from './components/Menucomponents';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       
         <Navbar dark color ="primary">
           <div className="container">
             <NavbarBrand href="/">Restaurant</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
+        
      
     </div>
   );
